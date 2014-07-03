61c50e01fa77181121fa9f1a3d2f7fffa36d40b0
Juan Luis Martínez <juanlu@comakai.com>
2014-07-03 17:16:12 +0200

    Check config

--- config/config-example.js
+++ config/config-example.js
@@ -5,7 +5,7 @@ config.server.port = 8008;
 
 config.mysql = {};
 config.mysql.host = "localhost";
-config.mysql.database = "qnoow-chat";
+config.mysql.database = "qnoow";
 config.mysql.user = "root";
 config.mysql.password = "";
 
