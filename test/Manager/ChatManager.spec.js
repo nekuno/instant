9292ce13e2b698f467fd8b0163abe179b5b0229d
Juan Luis Martínez <juanlu@comakai.com>
2014-07-07 16:14:44 +0200

    gulp & gulp-jasmine for testing

---
+++ test/Manager/ChatManager.spec.js
@@ -0,0 +1,13 @@
+var ChatManager = require('./../../src/Manager/ChatManager');
+
+describe('ChatManager', function () {
+
+    // TODO: Mock this dependencies
+    // var chatManager = new ChatManager;
+
+    it('should do something', function (done) {
+
+        expect(null).toBe(null);
+        done();
+    });
+});
