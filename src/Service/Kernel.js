c40e20a8813f0120b022c93f59d73f02487b055f
Juan Luis Martínez <juanlu@comakai.com>
2014-10-21 19:02:59 +0200

    QS-329: Socket by https optional

--- src/Service/Kernel.js
+++ src/Service/Kernel.js
@@ -1,8 +1,22 @@
 var Kernel = function(container) {
 
     var params = container.get('params');
+    var server;
 
-    var server = require('http').createServer();
+    if (params.server.type === 'http') {
+
+        server = require(params.server.type).createServer();
+
+    } else if (params.server.type === 'https') {
+
+        var fs = require('fs');
+        var options = {
+            key : fs.readFileSync(params.server.key),
+            cert: fs.readFileSync(params.server.cert)
+        };
+        server = require(params.server.type).createServer(options);
+
+    }
     server.listen(params.server.port);
 
     var io = require('socket.io').listen(server);
