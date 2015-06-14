02e16a0019d2cf9e1fb3828cc570c4430835917a
Juan Luis Martínez <juanlu@comakai.com>
2015-06-14 12:28:27 +0200

    QS-340: Images

--- src/Model/User.js
+++ src/Model/User.js
@@ -9,7 +9,9 @@ var User = function(bookshelf) {
                 username: user.get('username'),
                 email   : user.get('email'),
                 image   : {
-                    profile: user.get('picture') ? '/media/cache/resolve/profile_picture/user/images/' + user.get('picture') : '/media/cache/resolve/profile_picture/bundles/qnoowweb/images/user-no-img.jpg'
+                    profile: user.get('picture') ? '/media/cache/resolve/profile_picture/user/images/' + user.get('picture') : '/media/cache/resolve/profile_picture/bundles/qnoowweb/images/user-no-img.jpg',
+                    small: user.get('picture') ? '/media/cache/resolve/user_avatar_60x60/user/images/' + user.get('picture') : '/media/cache/resolve/user_avatar_60x60/bundles/qnoowweb/images/user-no-img.jpg',
+                    medium: user.get('picture') ? '/media/cache/resolve/user_avatar_180x180/user/images/' + user.get('picture') : '/media/cache/resolve/user_avatar_180x180/bundles/qnoowweb/images/user-no-img.jpg'
                 }
             }
         }
