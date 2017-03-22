759eeab607d4bed421552cfa90bcf67e3ae1bc31
msalsas <manolez@gmail.com>
2017-03-22 15:19:20 +0100

    QS-1331: Fix user photo in message notificatios

--- src/Manager/ChatSocketManager.js
+++ src/Manager/ChatSocketManager.js
@@ -160,7 +160,9 @@ ChatSocketManager.prototype.add = function(socket) {
                                                 message.user = user;
                                                 socket.emit('messages', [message], true);
                                                 var title = user_to.locale === 'es' ? 'Nuevo mensaje' : 'New message';
-                                                var icon = user_from.thumbnail && user_from.thumbnail.small ? user_from.thumbnail.small : user_from.url ? user_from.url : null;
+                                                var icon = user_from.photo && user_from.photo.thumbnail && user_from.photo.thumbnail.small ?
+                                                    user_from.photo.thumbnail.small
+                                                    : user_from.photo && user_from.photo.url ? user_from.photo.url : null;
                                                 var body = user_to.locale === 'es' ? user_from.username + ' te ha enviado un mensaje.'
                                                     : user_from.username + ' has just sent you a message.' ;
                                                 self.notificationsSocketManager.message(userTo, user_from.slug, title, body, user_to.locale, icon);
